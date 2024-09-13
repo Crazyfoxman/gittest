c3893c1cead51485faa80805de41628c454f9a2c
CrazyFox <echidnariz@gmail.com>
2024-09-13 21:12:02 +0300

    Парсер куки состояния

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = 'https://browser-info.ru'
+link = "https://browser-info.ru"
 
-parshtml = requests.get(link).text
-answer = BeautifulSoup(parshtml, 'lxml')
-soup = answer.find('div', id='tool_padding')
-flash = soup.find('div', id='flash_version')
-status = flash.find_all('span')[1].text
+html_code = requests.get(link).text
+soup = BeautifulSoup(html_code, "lxml")
+block = soup.find('div', id="tool_padding")
+cookie = block.find('div', id="cookie_check")
+status = cookie.find_all('span')[1].text
+
 print(status)
